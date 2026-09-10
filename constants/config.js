5deb79a826b8766456131a9cf119b3b361af83c3
Jignesh <140388708+Jigneshkotia@users.noreply.github.com>
2026-09-11 01:14:26 +0530

    Update config.js for cors errors

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,10 @@
 
 const corsOptions = {
-    origin :'https://www.heybuddy.live',
+    origin : [
+        "https://www.heybuddy.live",
+        "https://chattingkaro-client.vercel.app",
+        "https://www.chattingkaro.live",
+    ],
     methods : ["GET","POST","PUT","DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials : true
